d96a1459d2b07196eb8ef586659730430256394b
raphealshirley <shirleyli8881@gmail.com>
2020-06-17 12:41:32 -0400

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { adminRouter } from './routes'
 import {
     Button
 } from 'antd'
@@ -22,7 +23,26 @@ class App extends Component {
     render() {
         return (
             <div>
-                App <Button>ANTD</Button>
+                <div>公共部分</div>
+                <Switch>
+                    {
+                        adminRouter.map(route => {
+                            return (
+                                <Route
+                                    key={route.pathname}
+                                    path={route.pathname}
+                                    exact={route.exact}
+                                    render={(routerProps) => {
+                                        return <route.component {...routerProps} />
+                                    }}
+                                />
+                            )
+                        })
+                    })
+
+                <Redirect to={adminRouter[0].pathname} from="/admin" exact />
+                    <Redirect to="/404" />
+                </Switch>
             </div>
         )
     }
