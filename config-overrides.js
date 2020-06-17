c4e480bf5840a731e48894703c59f5f104e2db8d
raphealshirley <shirleyli8881@gmail.com>
2020-06-16 23:23:28 -0400

    Based on cra and react-app-rewired and customize

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+/*
+* @file config-overrides.js
+* @author shirley
+* Based on customize and react-app-rewired config
+*/
+
+// Introduce methods from customize-cra
+const { override } = require('customize-cra')
+module.exports = override()
